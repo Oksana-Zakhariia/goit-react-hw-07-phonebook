e0be8c530543af617dabf6bd5252828a1aa2e262
Oksana-Zakhariia <okcahka.l@gmail.com>
2023-02-26 15:18:59 +0300

    fixed

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -3,21 +3,15 @@ import { Contact } from 'components/Contact/Contact';
 import { getContacts, getFilter } from 'redux/selectors';
 import { List } from './ContactList.styled';
 const getVisibleContacts = (contacts, filter) => {
-  const visibleContacts = [];
-  contacts.filter(contact => {
-    if (contact.name.includes(filter.toLowerCase())) {
-      return visibleContacts.push(contact);
-    }
-    return visibleContacts;
-  });
-  return visibleContacts;
+  return contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 };
 
 export const ContactList = () => {
   const contacts = useSelector(getContacts);
   const filter = useSelector(getFilter);
-  // console.log(typeof filter);
-  // console.log(contacts);
+
   const visibleContacts = getVisibleContacts(contacts, filter);
 
   return (
